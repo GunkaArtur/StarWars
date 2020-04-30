28e9d413abe44e3251d013dc9573c2ac9c20ade1
Artur Gunka <arturel101010@mail.ru>
2020-04-30 15:14:01 +0300

    Errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 const getResourse = async (url) => {
     const res = await  fetch(url);
+    if(!res.ok){
+        throw new Error(`Could not fetch ${url} , received ${res.status}`, )
+    }
     const body = await res.json();
     return body
 }
@@ -8,6 +11,9 @@ const getResourse = async (url) => {
 getResourse("https://swapi.dev/api/people/1/")
     .then((body)=>{
         console.log(body);
+    })
+    .catch((err)=>{
+        console.log("Could not fetch", err)
     })
 
 
